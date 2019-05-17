feeaf247bd594b1c293b988b8d9ada71f05dae14
Robert K. Bell <robert.bell@envoyat.com>
2019-05-17 16:20:29 +1000

    feat: add reasonable scroll sync

--- src/main.js
+++ src/main.js
@@ -8,14 +8,55 @@ const expectedTextRegex = new RegExp(
 const expectedElement = document.getElementById('text_output_expected')
 const actualElement = document.getElementById('text_output_actual')
 
+const sync = debounce(function _sync(bool) {
+  if (bool) {
+    expectedElement.scrollTop = actualElement.scrollTop
+  } else {
+    actualElement.scrollTop = expectedElement.scrollTop
+  }
+}, 20, true)
+
+expectedElement.addEventListener('scroll', debounce(select_scroll_1, 100, false), false)
+actualElement.addEventListener('scroll', debounce(select_scroll_2, 100, false), false)
+
+function select_scroll_1() {
+  console.log('s1')
+  sync(false)
+}
+
+function select_scroll_2() {
+  console.log('s2')
+  sync(true)
+}
+
 function processText(textAreaElement) {
   const rawText = textAreaElement.value
 
-  const execd = expectedTextRegex.exec(rawText) || []
-  const expectedText = (execd)[1]
-  const actualText = (execd)[3]
-  console.info('!', {execd, expectedText, actualText})
+  const results = expectedTextRegex.exec(rawText) || []
+  const expectedText = results[1]
+  const actualText = results[3]
+  console.info('!', {results, expectedText, actualText})
 
   expectedElement.value = expectedText
   actualElement.value = actualText
+}
+
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  let timeout
+  return function () {
+    const context = this, args = arguments
+    const later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
 }
